70221997b88b05b82e629232271772458e35bc96
Camilo <camilo.molano@arkdia.co>
2022-03-10 10:20:49 -0500

    Add checkEnginehistory

--- contracts/car.sol
+++ contracts/car.sol
@@ -6,19 +6,29 @@ contract Auto{
 
     struct autoTemplate{
         address owner;
+        string [] checkEngine;
         string brand;
         uint256 year;
         string color;
         bool status;
         bool validKey;
+        
     }
 
     mapping(string => autoTemplate) public autosCollection;
 
     
-    function createCar(string memory _plate, string memory _brand, uint256 _year, string memory _color)public{
+    function createCar(string memory _plate,string memory _date, string memory _brand, uint256 _year, string memory _color)public{
         require(!autosCollection[_plate].validKey, "Car already exits");
-        autosCollection[_plate] = autoTemplate(msg.sender, _brand, _year, _color, true, true);
+        //autosCollection[_plate] = autoTemplate(msg.sender,_brand, _year, _color, true, true);
+        autosCollection[_plate].owner = msg.sender;
+        autosCollection[_plate].checkEngine.push(_date); 
+        autosCollection[_plate].brand = _brand;
+        autosCollection[_plate].year = _year;
+        autosCollection[_plate].color = _color;
+        autosCollection[_plate].status = true;
+        autosCollection[_plate].validKey = true;
+
     }
 
     function updateCar(string memory _plate, string memory _color)public{
@@ -36,6 +46,20 @@ contract Auto{
         require(msg.sender == autosCollection[_plate].owner);
         autosCollection[_plate].owner = to;
 
+    }
+
+    function addcheckEngineHistory(string memory _plate, string memory _date)public {
+        require(autosCollection[_plate].status, "Car is not in circulation state");
+        require(autosCollection[_plate].validKey, "Car do not exits");
+        autosCollection[_plate].checkEngine.push(_date);
+
+    }
+        
+    function checkEngineHistory(string memory _plate)public view returns(string[] memory){
+        require(autosCollection[_plate].status, "Car is not in circulation state");
+        require(autosCollection[_plate].validKey, "Car do not exits");
+        return autosCollection[_plate].checkEngine;
+        
     }
 
 }
